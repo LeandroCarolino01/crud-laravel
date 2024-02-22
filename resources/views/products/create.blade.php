b0a760f81fa0b537bb3526de269c13d31d11752c
Leandro.Carolino <carolino.leandro@gmail.com>
2024-02-22 23:50:26 +0100

    change view

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>Create product</h1>
+    <form method="post" action="">
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="name" />
+        </div>
+        <div>
+            <label>Qty</label>
+            <input type="text" name="qty" placeholder="qty" />
+        </div>
+        <div>
+            <label>Price</label>
+            <input type="text" name="price" placeholder="price" />
+        </div>
+        <div>
+            <label>Description</label>
+            <input type="text" name="description" placeholder="Description" />
+        </div>
+        <div>
+            <label>Test</label>
+            <input type="text" name="description" placeholder="Description" />
+        </div>
+        <div>
+            <input type="submit" value="Save a New Product">
+        </div>
+    </form>
 </body>
 </html>
